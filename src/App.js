ccd29b1d2c5548e9d1f7e8233f84ce932b5a5d57
Ariful islam <ariful@icodelabs.co>
2021-09-22 01:28:32 +0600

    feat: carouse dots

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState } from 'react';
 import CarouselWrapper from './components/CarouselWrapper';
+import CarouselDots from './components/CarouselDots';
 
 import slidesToShow from './plugins/slidesToShow';
 
 // import './App.css';
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  const slides = [
+    <img
+      src='https://brainhubeu.github.io/react-carousel/static/mona-7a1ceae9bdb8c43272eb101c091c5408.jpg'
+      alt=''
+    />,
+    <img
+      src='https://brainhubeu.github.io/react-carousel/static/mona-7a1ceae9bdb8c43272eb101c091c5408.jpg'
+      alt=''
+    />,
+    <img
+      src='https://brainhubeu.github.io/react-carousel/static/mona-7a1ceae9bdb8c43272eb101c091c5408.jpg'
+      alt=''
+    />,
+  ];
+
   return (
     <div className='App'>
       <CarouselWrapper
@@ -17,20 +36,15 @@ function App() {
             },
           },
         ]}
-      >
-        <img
-          src='https://brainhubeu.github.io/react-carousel/static/mona-7a1ceae9bdb8c43272eb101c091c5408.jpg'
-          alt=''
-        />
-        <img
-          src='https://brainhubeu.github.io/react-carousel/static/mona-7a1ceae9bdb8c43272eb101c091c5408.jpg'
-          alt=''
-        />
-        <img
-          src='https://brainhubeu.github.io/react-carousel/static/mona-7a1ceae9bdb8c43272eb101c091c5408.jpg'
-          alt=''
-        />
-      </CarouselWrapper>
+        slides={slides}
+        value={value}
+        onChange={(v) => setValue(v)}
+      />
+      <CarouselDots
+        value={value}
+        onChange={(v) => setValue(v)}
+        number={slides?.length}
+      />
     </div>
   );
 }
